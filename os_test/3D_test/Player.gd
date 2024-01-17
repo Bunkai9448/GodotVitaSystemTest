cba757bce98432a818ddcd340952022c10212509
Bunkai9448 <71356234+Bunkai9448@users.noreply.github.com>
2024-01-17 23:14:51 +0100

    More preparation for 3D test

---
+++ os_test/3D_test/Player.gd
@@ -0,0 +1,23 @@
+extends RigidBody
+
+
+onready var raycast = get_node("RayCast")
+onready var start_position = translation
+
+
+func _physics_process(_delta):
+	var dir = Vector3()
+	dir.x = Input.get_action_strength("ui_left") - Input.get_action_strength("ui_right")
+	dir.z = Input.get_action_strength("ui_up") - Input.get_action_strength("ui_down")
+
+	apply_central_impulse(dir.normalized() /10)
+
+	# Jumping code.
+	if on_ground() and Input.is_action_pressed("jump"):
+		apply_central_impulse(Vector3.UP)
+
+
+# Test if there is a body below the player.
+func on_ground():
+	if raycast.is_colliding():
+		return true
