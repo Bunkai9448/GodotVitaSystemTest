43fb20a8b484afbf8b82b4eace7b87ada85466a7
Bunkai9448 <71356234+Bunkai9448@users.noreply.github.com>
2024-02-07 12:35:42 +0100

    Getting ready for external files in Audio_test

--- os_test/AudioTest.gd
+++ os_test/AudioTest.gd
@@ -1,11 +1,28 @@
 extends Node
 
 
+onready var batFile = "file.bat"
+onready var blocking = false
+onready var path = str(get_path()) + '&&'
+onready var args = ['/C', 'start', path, batFile]
+onready var output = [path]
+
+onready var CorrectSound = preload("res://Button.mp3")
+
+
 func _ready():
 	pass # Replace with function body.
 
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
 func _on_AudioButton_pressed():
-	if !$AudioStreamPlayer.playing:
+#	OS.shell_open(batFile)
+#		OS.execute('cmd',args, blocking ,output)
+	if !$AudioStreamPlayer.is_playing():
+		$AudioStreamPlayer.stream = CorrectSound
 		$AudioStreamPlayer.play()
-	else:
-		$AudioStreamPlayer.stop()	
+	
+	else: 
+		$AudioStreamPlayer.stop()
